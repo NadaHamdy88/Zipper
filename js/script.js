f8c07a5199be85bf8eb92a9bc8f627cd9179962e
nadahamdy88 <nada.hamdy7782@gmail.com>
2019-03-26 17:29:12 +0200

    query

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    
+    $(window).scroll(function(){
+        var scroll = $(this).scrollTop();
+        if( scroll > 200 ){
+            $(".scroll_nav").slideDown();
+        }
+        else{
+            $(".scroll_nav").slideUp();
+
+        }
+    });
+
+    $(".header li a").click(function(){
+        $("html , body").animate({
+            scrollTop : $("#" + $(this).data("value")).offset().top
+        },1500);
+
+    });
+});
